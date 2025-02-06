985f01d5f828ddfbb04fd84725d7da031e6243b9
pemiinem <190139323@aluno.unb.br>
2025-02-06 20:23:50 -0300

    fourth submission (accepted)

--- 32_longest_valid_parentheses.py
+++ 32_longest_valid_parentheses.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        stack = [0]
+        stack = [-1]
         max_length = 0
 
         for i, char in enumerate(s):
@@ -11,6 +11,6 @@ class Solution:
                 if not stack:
                     stack.append(i)
                 else:
-                    max_length = max(max_length, i - stack[1])
+                    max_length = max(max_length, i - stack[-1])
 
         return max_length
